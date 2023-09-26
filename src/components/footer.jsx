fd272e26dfcec4d1697341962d96477de85193f2
BradyLange1 <bradylange99@gmail.com>
2023-09-26 17:23:25 -0500

    added public folder

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -2,9 +2,9 @@ export default function Footer() {
   return (
     <>
       <div className="footer">
-        <a  href="https://github.com/BradyLange1"><img className="footerImg" src="src\assets\GitHubCat.png" alt="gitHub logo" /></a>
-        <a  href="#"><img className="footerImg" src="src\assets\linkedin-icon.png" alt="linkedin logo" /></a>
-        <a  href="#"><img className="footerImg" src="src\assets\twiterLogo.png" alt="stack overflow logo" /></a>
+        <a  href="https://github.com/BradyLange1"><img className="footerImg" src="/assets/GitHubCat.png" alt="gitHub logo" /></a>
+        <a  href="#"><img className="footerImg" src="/assets/linkedin-icon.png" alt="linkedin logo" /></a>
+        <a  href="#"><img className="footerImg" src="/assets/twiterLogo.png" alt="stack overflow logo" /></a>
       </div>
     </>
   );
--- src/components/project.jsx
+++ src/components/project.jsx
@@ -6,10 +6,10 @@ export default function Project(props) {
         <h3 className="projectName">{props.name}</h3>
         <div className="projectLinks">
           <a href={props.urlLink} className="projectLinkURL">
-            <img src="src\assets\pageLink.png" alt="" />
+            <img src="/assets/pageLink.png" alt="" />
           </a>
           <a href={props.gitHubLink} className="projectLinkGitHub">
-            <img src="src/assets/GitHubCat.png" alt="" />
+            <img src="/assets/GitHubCat.png" alt="" />
           </a>
         </div>
       </div>
