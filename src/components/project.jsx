672e9d730b7cff0b4b76d98396be05e4bceea3fb
BradyLange1 <bradylange99@gmail.com>
2024-01-16 14:01:36 -0600

    added alt tags to img in project component

--- src/components/project.jsx
+++ src/components/project.jsx
@@ -6,10 +6,10 @@ export default function Project(props) {
         <h3 className="projectName">{props.name}</h3>
         <div className="projectLinks">
           <a href={props.urlLink} className="projectLinkURL">
-            <img src="/assets/pageLink.png" alt="" />
+            <img src="/assets/pageLink.png" alt="page-link-image" />
           </a>
           <a href={props.gitHubLink} className="projectLinkGitHub">
-            <img src="/assets/GitHubCat.png" alt="" />
+            <img src="/assets/GitHubCat.png" alt="github-link-image" />
           </a>
         </div>
       </div>
